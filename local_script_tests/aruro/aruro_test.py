fb926e41335b0c9b149ea730e457485012d10c9f
mikesmalbroek <mikesmalbroek@hotmail.com>
2026-05-27 10:08:43 +0200

    fixed the name of aruro and organized the local digital screen algorithms in their folder

--- local_script_tests/aruro/aruro_test.py
+++ local_script_tests/aruro/aruro_test.py
@@ -38,7 +38,7 @@ dist_coeffs = np.zeros(5)
 marker_length = 0.15
 
 # ===== LOAD PHOTO =====
-frame = cv2.imread('local_script_tests/aruco/photos_for_aruco/stationA_skewed_02.png')
+frame = cv2.imread('src/cognitive_robot/local_script_tests/aruco/photos_for_aruco/stationA_skewed_02.png')
 
 # Check if the image loaded successfully
 if frame is None:
